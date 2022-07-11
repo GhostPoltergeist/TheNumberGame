a7ca3439305133860bc5f9a92c7b153800776f5e
pitzzahh <araopj2191@gmail.com>
2022-07-11 14:25:31 +0800

    fixed all the bugs.

--- program/game/start/NumberGame.java
+++ program/game/start/NumberGame.java
@@ -1,72 +1,79 @@
 package program.game.start;
 
+import java.util.Random;
+
 import static program.game.start.GameUtil.getComputerBet;
 import static program.game.start.GameUtil.*;
 
 public class NumberGame {
 
     public static void main(String[] args) throws InterruptedException {
-        start(new Health(5));
+        start(new Health(5), false);
     }
 
-    public static void start(Health health) throws InterruptedException {
+    public static void start(Health health, boolean doesWonWhenLost) throws InterruptedException {
         getBannerStyle();
         //Users::Input::Computers::Random
         System.out.println(PURPLE_BOLD_BRIGHT + "|> Random First Bet <|" + RESET);
-        Thread.sleep(1500);
+        Thread.sleep(new Random().nextInt(1000) + 1);
         int firstPick = (int) (Math.random() * 2) + 1;
-        Thread.sleep(1000);
+        Thread.sleep(new Random().nextInt(500) + 1);
 
         if (firstPick == 1) {
             System.out.println(RED_BOLD_BRIGHT + "|> You're the First One Who Will Pick A Number <|" + RESET);
-            getResult(health, true);
+            getResult(doesWonWhenLost && health.remainingHealth() < 5 ? new Health(health.remainingHealth() + 1) : health, true, doesWonWhenLost);
         }
         else if (firstPick == 2) {
             System.out.println(RED_BOLD_BRIGHT + "|> You're the Last One Who Will Pick A Number <|" + RESET);
-            getResult(health, false);
+            getResult(doesWonWhenLost && health.remainingHealth() < 5 ? new Health(health.remainingHealth() + 1) : health, false, doesWonWhenLost);
         }
         //Conditionals::SeparatedMethods
     }
 
     public static void lose(Health health) throws InterruptedException {
 
-        /* String Question for Loops */
-
-        gameLose(true, health.remainingHealth() - 1);
+        health = gameLose(true, health.remainingHealth() - 1, false);
+        isOutOfLives(health);
+        int computerBet = getComputerBet();
+        int userBet = getUserBet();
 
         //Conditionals::userBet <-> computerBet
-        if (getUserBet() > getComputerBet()) win(false, health); /* Win */
-
-        if (getUserBet() < getComputerBet()) /* Lose */ {
-
-            gameLose(true, health.remainingHealth() - 1);
+        if (userBet > computerBet) win(true, new Health(health.remainingHealth() + 1), true); /* Win */
+        else if (userBet < computerBet)  { /* Lose */
+            health = gameLose(true, health.remainingHealth() - 1, false);
+            isOutOfLives(health);
+            computerBet = getComputerBet();
+            userBet = getUserBet();
 
             //Conditionals::userBet <-> computerBet
-            if (getUserBet() > getComputerBet()) win(false, new Health(health.remainingHealth() + 1)); /* Win */
+            if (userBet > computerBet) win(true, new Health(health.remainingHealth() + 1), true); /* Win */
+            else if (userBet < computerBet) { /* Lose */
 
-            if (getUserBet() < getComputerBet()) /* Lose */ {
-
-                gameLose(true, health.remainingHealth() - 2);
+                health = gameLose(true, health.remainingHealth() - 1, false);
+                isOutOfLives(health);
+                computerBet = getComputerBet();
+                userBet = getUserBet();
 
                 //Conditionals::userBet <-> computerBet
-                if (getUserBet() > getComputerBet()) win(false, new Health(health.remainingHealth() + 1)); /* Win */
+                if (userBet > computerBet) win(true, new Health(health.remainingHealth() + 1), true); /* Win */
+                else if (userBet< computerBet) /* Lose */ {
 
-                if (getUserBet() < getComputerBet()) /* Lose */ {
-
-                    gameLose(true, health.remainingHealth() - 3);
+                    health = gameLose(true, health.remainingHealth() - 1, false);
+                    computerBet = getComputerBet();
+                    userBet = getUserBet();
 
                     //Conditionals::userBet <-> computerBet
-                    if (getUserBet() > getComputerBet()) win(false, new Health(health.remainingHealth() + 1)); /* Win */
-
-                    if (getUserBet() < getComputerBet()) /* Lose */ {
-                        System.out.println("\n" + RED_BOLD_BRIGHT + ">| Out Of Lives <|" + RESET);
-                        getResponse(health);
+                    if (userBet > computerBet) win(true, new Health(health.remainingHealth() + 1), true); /* Win */
+                    else if (userBet < computerBet) {
+                        health = gameLose(true, health.remainingHealth() - 1, false);
+                        isOutOfLives(health);
                     }
-                } else if (getUserBet() > getComputerBet()) win(false, new Health(health.remainingHealth() + 1)); /* Win */
-
+                }
+                if (userBet == computerBet) tie(false, health, false); /* Tie */
             }
+            if (userBet == computerBet) tie(false, health, false); /* Tie */
         }
-        if (getComputerBet() == getComputerBet()) tie(false, health); /* Tie */
+        if (userBet == computerBet) tie(false, health, false); /* Tie */
     }
 
 }
