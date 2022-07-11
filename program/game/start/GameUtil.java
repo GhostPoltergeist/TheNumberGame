a7284b3a17840cc5e49a6e5774d96e546c881047
pitzzahh <araopj2191@gmail.com>
2022-07-11 13:32:42 +0800

    changed Thread.sleep() argument to a random number.

--- program/game/start/GameUtil.java
+++ program/game/start/GameUtil.java
@@ -10,7 +10,7 @@ public class GameUtil extends Util {
      * @param playAgain if needed to input to play again.
      * @throws InterruptedException if interrupted at some point.
      */
-    public static void win(boolean playAgain, Health health) throws InterruptedException {
+    public static void win(boolean playAgain, Health health, boolean doesWonWhenLost) throws InterruptedException {
 
         System.out.println("\n" + RED_BOLD_BRIGHT + "  |> WIN! <|" + RESET);
         System.out.println(RED_BOLD_BRIGHT + "|> HEALTH: " + health.remainingHealth() + " <|" + RESET);
@@ -18,16 +18,16 @@ public class GameUtil extends Util {
 
         System.out.println("\n" + WIN_TEXT[new Random().nextInt(WIN_TEXT.length)]);
 
-        if (playAgain) getResponse(health);
+        if (playAgain) getResponse(health, doesWonWhenLost);
     }
 
     /**
      * Informs the user that he/she lose, but continue the game.
      * @throws InterruptedException if interrupted at some point.
      */
-    public static void gameLose(boolean isLosing, int healthCount) throws InterruptedException {
+    public static Health gameLose(boolean isLosing, int healthCount, boolean doesWonWhenLost) throws InterruptedException {
+        Health health = new Health(healthCount);
         if (isLosing){
-            Health health = new Health(healthCount);
             System.out.println(RED_BOLD_BRIGHT + "  |> LOSE <|" + RESET);
             System.out.println(RED_BOLD_BRIGHT + "|> HEALTH: " + health.remainingHealth() + " <|" + RESET);
 
@@ -35,35 +35,36 @@ public class GameUtil extends Util {
             System.out.println("\n" + LOSE_TEXT[new Random().nextInt(LOSE_TEXT.length)]);
 
             //Sleep::Continue::Game:Start
-            Thread.sleep(500);
+            Thread.sleep(new Random().nextInt(500) + 1);
             //Sleep::Continue::Game::End
         }
         else {
             System.out.println("\n" + RED_BOLD_BRIGHT + "                         |> LOSE <|" + RESET);
             System.out.println(RED_BOLD_BRIGHT + " |> Lifesaver is Here! you have another chance to win <|" + RESET);
             //Break::Continue
-            NumberGame.start(new Health(healthCount));
+            NumberGame.start(new Health(healthCount), doesWonWhenLost);
         }
+        return health;
     }
 
-    public static void tie(boolean tryAgain, Health health) throws InterruptedException {
+    public static void tie(boolean tryAgain, Health health, boolean doesWonWhenLost) throws InterruptedException {
         /* String Question for Loops */
-        System.out.println("\n" + RED_BOLD_BRIGHT + " |> TIE SCORES <|" + RESET);
+        System.out.println("\n" + RED_BOLD_BRIGHT + "|> TIE SCORES <|" + RESET);
         System.out.println(RED_BOLD_BRIGHT + "|> HEALTH: " + health.remainingHealth() + " <|" + RESET);
         //GameNotice
 
         System.out.println("\n" + TIE_TEXT[new Random().nextInt(TIE_TEXT.length)]);
-        if (tryAgain) getResponse(health);
+        if (tryAgain) getResponse(health, doesWonWhenLost);
     }
 
-    public static void getResponse(Health health) throws InterruptedException {
-        System.out.print(RED_BOLD_BRIGHT + "|> [Y/N]Play Again?: " + RESET);
+    public static void getResponse(Health health, boolean doesWonWhenLost) throws InterruptedException {
+        System.out.print(RED_BOLD_BRIGHT + "|> [Y/N] Play Again?: " + RESET);
         String answer = INPUT.next();
-        if (answer.matches("^[Y|y]$")) NumberGame.start(new Health(health.remainingHealth()));
+        if (answer.matches("^[Y|y]$")) NumberGame.start(new Health(health.remainingHealth()), doesWonWhenLost);
         if (answer.matches("^[N|n]$")) System.exit(0);
     }
 
-    public static void getResult(Health health, boolean isUserTheFirstOneToBet) throws InterruptedException {
+    public static void getResult(Health health, boolean isUserTheFirstOneToBet, boolean doesWonWhenLost) throws InterruptedException {
         //Conditionals::userBet <-> computerBet
         int userBet;
         int computerBet;
@@ -77,14 +78,14 @@ public class GameUtil extends Util {
             userBet = getUserBet();
         }
 
-        if (userBet > computerBet) win(true, health);    /*  Win  */
-        if (userBet < computerBet) lose(health);         /*  Lose */
-        if (userBet == computerBet) tie(true, health);   /*  Tie  */
+        if (userBet > computerBet) win(true, health, doesWonWhenLost);   /*  Win  */
+        else if (userBet < computerBet) lose(health);   /*  Lose */
+        else tie(true, health, doesWonWhenLost);                         /*  Tie  */
     }
 
     public static int getComputerBet() throws InterruptedException {
         System.out.println(RED_BOLD_BRIGHT + "|> Please Wait, Computer is now picking <|" + RESET);
-        Thread.sleep(2000);
+        Thread.sleep(new Random().nextInt(1000) + 1);
         int bet = (int) (Math.random() * 999) + 1;
         System.out.println("BET: " + bet);
         return bet;
@@ -92,7 +93,7 @@ public class GameUtil extends Util {
 
     public static int getUserBet() throws InterruptedException {
         System.out.println(RED_BOLD_BRIGHT + "|> Pick A Number <|" + RESET);
-        Thread.sleep(1500);
+        Thread.sleep(new Random().nextInt(1000) + 1);
         System.out.print(CYAN_BOLD_BRIGHT + "|> Please Bet A Number: " + RESET);
         return (INPUT.nextInt());
     }
